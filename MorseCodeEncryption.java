f2bc82acd999365ef805655cf5c3b7f496eac172
DigantaD <diggyhacker.coder@gmail.com>
2017-12-10 11:55:09 +0530

    Update MorseCodeEncryption.java

--- MorseCodeEncryption.java
+++ MorseCodeEncryption.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.*;
 import java.lang.*;
- public class Main{
-  public static void main(String args[])
+ public class MorseCodeEncryption{
+ public static void main(String args[])
 {
     Scanner sc= new Scanner(System.in);
     System.out.print("Enter text: ");
